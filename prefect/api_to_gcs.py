b51d4cd030bac374bae52c1a33349486421ee86f
Vincenzo <mr.vincenzo.galante@gmail.com>
2023-03-10 09:35:16 +0000

    added timestamp column and tested data types of dataset within data lake

--- prefect/api_to_gcs.py
+++ prefect/api_to_gcs.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+import datetime as dt
 from glom import glom
 from pathlib import Path
 from prefect import task, flow
@@ -13,7 +14,7 @@ def fetch_cards(url: str) -> pd.DataFrame:
     if response.status_code == 200:
         json = response.json()["download_uri"]
         update = response.json()["updated_at"]
-        df = pd.read_json(json)
+        df = pd.read_json(json).fillna("")
         print("Successfully fetched data from API.")
         return df, update
     else:
@@ -23,21 +24,29 @@ def fetch_cards(url: str) -> pd.DataFrame:
 @task(log_prints=True, name="Select columns and enforce data type")
 def enforce_col_types(df: pd.DataFrame) -> pd.DataFrame:
     """Selects the columns we want to keep and enforces the correct data types"""
+
+    df["id"] = df["id"].astype("string")
     df["name"] = df["name"].astype("string")
     df["released_at"] = pd.to_datetime(df["released_at"])
 
+    df["colors"] = df["colors"].apply(", ".join)
     df["colors"] = df["colors"].astype("string")
-    # df["colors"] = df["colors"].apply(", ".join)
 
+    df["color_identity"] = df["color_identity"].apply(", ".join)
     df["color_identity"] = df["color_identity"].astype("string")
+
     df["set_name"] = df["set_name"].astype("string")
     df["artist"] = df["artist"].astype("string")
 
     euro_prices = df["prices"].apply(lambda row: glom(row, "eur", default=None))
     df["prices"] = euro_prices.astype(float)
 
+    date = dt.datetime.now()
+    df["data_update"] = date
+
     df = df[
         [
+            "id",
             "name",
             "released_at",
             "colors",
@@ -45,6 +54,7 @@ def enforce_col_types(df: pd.DataFrame) -> pd.DataFrame:
             "set_name",
             "artist",
             "prices",
+            "data_update",
         ]
     ]
     print("Successfully selected columns and enforced data types.")
@@ -52,33 +62,37 @@ def enforce_col_types(df: pd.DataFrame) -> pd.DataFrame:
 
 
 @task(log_prints=True, name="Write to local parquet")
-def write_to_local_parquet(df: pd.DataFrame, update: str) -> Path:
+def write_to_local_parquet(df: pd.DataFrame, update: str, dataset: str) -> Path:
     """Writes the dataframe to a local parquet file"""
     path = Path(__file__).parent.parent
-    local_path = Path(f"{path}/pq/oracle_{update}.parquet")
+    local_path = Path(f"{path}/pq/{dataset}_{update}.parquet")
     df.to_parquet(local_path, compression="gzip")
     print(f"Successfully wrote to local parquet: {local_path}")
     return local_path
 
 
-def write_to_gcs(local_path: str, update: str) -> None:
+@task(log_prints=True, name="Write to GCS")
+def write_to_gcs(local_path: str, update: str, dataset: str) -> None:
     """Writes a local parquet file to GCS"""
-    gcs_path = Path(f"pq/oracle_{update}.parquet")
+    gcs_path = Path(f"pq/{dataset}_{update}.parquet")
     gcp_cloud = GcsBucket.load("magic-the-gathering-bucket")
     gcp_cloud.upload_from_path(from_path=local_path, to_path=gcs_path)
     return
 
 
 @flow(log_prints=True, name="[Magic: The Gathering] API to Cloud Storage")
-def api_to_gcs_orchestration() -> None:
+def api_to_gcs_orchestration(data_set: str) -> None:
     """Orchestrates the flow of the API to Google Cloud Storage Bucket pipeline"""
 
-    oracle_url = f"https://api.scryfall.com/bulk-data/oracle-cards"
-    json, update = fetch_cards(oracle_url)
+    api = f"https://api.scryfall.com/bulk-data/{data_set}"
+    json, update_ts = fetch_cards(api)
     selected_columns = enforce_col_types(json)
-    parquet = write_to_local_parquet(selected_columns, update)
-    write_to_gcs(parquet, update)
+    parquet = write_to_local_parquet(selected_columns, update_ts, data_set)
+    write_to_gcs(parquet, update_ts, data_set)
+    print(f"Uploaded {len(selected_columns)} rows to GCS.")
 
 
 if __name__ == "__main__":
-    api_to_gcs_orchestration()
+    oracle_data_set = "oracle_cards"
+    default_data_set = "default_cards"
+    api_to_gcs_orchestration(oracle_data_set)
