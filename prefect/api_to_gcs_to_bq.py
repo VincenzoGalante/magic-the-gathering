b6edfed122869e4ec971d4cb1aee6f01ff0607b3
Vincenzo <mr.vincenzo.galante@gmail.com>
2023-03-20 09:58:37 +0000

    Corrected try and except statements

--- prefect/api_to_gcs_to_bq.py
+++ prefect/api_to_gcs_to_bq.py
@@ -21,7 +21,7 @@ def fetch_cards(url: str) -> pd.DataFrame:
         print("Successfully fetched data from API.")
         return df, update_ts
     else:
-        print(f"[ERROR] {response.status_code}")
+        print(f"[ERROR] {response.status_code}.")
 
 
 @task(log_prints=True, name="Write to Google Cloud Storage in parquet-format")
@@ -65,9 +65,12 @@ def concatenate(string: str) -> str:
 
 def select_euro(string: str) -> str:
     """Selects the usd price from the prices column"""
-    code = eval(string)
-    code = code["usd"]
-    return code
+    try:
+        code = eval(string)
+        code = code["usd"]
+        return code
+    except (NameError, SyntaxError):
+        return string
 
 
 @task(log_prints=True, name="Select columns and enforce data type")
@@ -79,12 +82,9 @@ def transform_df(path: str, update_ts: str) -> pd.DataFrame:
     df["id"] = df["id"].astype("string")
     df["name"] = df["name"].astype("string")
     df["released_at"] = pd.to_datetime(df["released_at"])
-
     df["color_identity"] = df["color_identity"].apply(concatenate)
-
     df["set_name"] = df["set_name"].astype("string")
     df["artist"] = df["artist"].astype("string")
-
     euro_prices = df["prices"].apply(select_euro)
     df["prices"] = euro_prices.astype(float)
 
@@ -139,15 +139,20 @@ def api_to_bq_orchestration(dataset: str, download_parquet: bool = False) -> Non
     write_to_gcs(api_df, update_ts, dataset, gcp_cloud)
     print(f"Uploaded {len(api_df)} rows to GCS.")
 
-    gcs_df = get_df_from_gcs(update_ts, dataset, gcp_cloud)
-    transformed_df = transform_df(gcs_df, update_ts)
+    try:
+        gcs_df = get_df_from_gcs(update_ts, dataset, gcp_cloud)
+        transformed_df = transform_df(gcs_df, update_ts)
 
-    if download_parquet == True:
-        transformed_df.to_parquet("magic-the-gathering/pq/sample_for_bq.parquet")
-        print(f"Downloaded {len(transformed_df)} rows and saved into a parquet.")
-    else:
-        write_to_bq(transformed_df)
-        print(f"Uploaded {len(transformed_df)} rows to BigQuery.")
+        if download_parquet == True:
+            transformed_df.to_parquet("magic-the-gathering/pq/sample_for_bq.parquet")
+            print(f"Downloaded {len(transformed_df)} rows and saved into a parquet.")
+        else:
+            write_to_bq(transformed_df)
+            print(f"Uploaded {len(transformed_df)} rows to BigQuery.")
+    except FileNotFoundError:
+        print("[ERROR] Parquet file for transformation not found in GCS.")
+    except (SyntaxError, NameError):
+        print("[ERROR] Invalid code syntax and/or variable not found.")
 
 
 if __name__ == "__main__":
